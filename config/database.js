a2150cec1b62a2e921e6c563cb650702f281fcbb
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-08-17 17:34:49 -0400

    middleware.js modified

--- config/database.js
+++ config/database.js
@@ -4,10 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI'),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
