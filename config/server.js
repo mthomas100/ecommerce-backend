180d23f7f1f79c27ebd575afa7c4bdccde55d041
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-08-17 17:53:34 -0400

    modified middleware and server

--- config/server.js
+++ config/server.js
@@ -1,3 +1,10 @@
 module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4a360982bcf12676d8ee7f58e5c490ea'),
+    },
+  },
   url: env('MY_HEROKU_URL'),
 });
