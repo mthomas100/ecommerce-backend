ff68c90c5528f3ce1ecae88e09d70a3e06c25fb6
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-08-17 18:53:27 -0400

    updated middleware

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
     cors: {
-      origin: ['http://localhost:1337', 'http://localhost:7777', 'https://ecommerce-strapi-backend.herokuapp.com'],
+      origin: ['http://localhost:1337', 'http://localhost:7777', 'https://ecommerce-strapi-backend.herokuapp.com', 'https://ecommerce-frontend-orcin.vercel.app/'],
     },
   },
 };
